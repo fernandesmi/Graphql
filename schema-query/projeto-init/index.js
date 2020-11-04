1dabbc14985021f51531dd8f556ca8a22d56853d
Millena Fernandes <millena.fernandes@driven.cx>
2020-11-04 15:57:56 -0300

    Resolver de tipo

--- schema-query/projeto-init/index.js
+++ schema-query/projeto-init/index.js
@@ -26,6 +26,12 @@ const typeDefs = gql`
 `
 
 const resolvers = {
+
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+    },
     Query: {
         ola() {
             return 'AAAAA'
@@ -39,7 +45,7 @@ const resolvers = {
                 nome: 'Ana da Web',
                 email: 'anadaweb@email.com',
                 idade: 23,
-                salario: 1234.56,
+                salario_real: 1234.56,
                 vip: true
             }
         }
